5ffe744a2738f802367f61f804c0ac001c79acdd
slowdony <435574793@qq.com>
2019-01-30 15:12:01 +0800

    ADD:SDRACTest

---
+++ SDRACTest/SDRACTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SDRACTest
+//
+//  Created by slowdony on 2019/1/30.
+//  Copyright © 2019年 slowdony. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SDRACTest/SDRACTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SDRACTest
+//
+//  Created by slowdony on 2019/1/30.
+//  Copyright © 2019年 slowdony. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
